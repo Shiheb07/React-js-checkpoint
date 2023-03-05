686826a1e75ad6f6e96edf0c8731ba03816aa967
Shiheb <shiheb007@gmail.com>
2023-03-05 12:08:39 +0100

    First commit

---
+++ learn-react/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import NavBar from './NavBar';
+import Forms from './Form';
+function App() {
+  return (
+    <div className="App">
+<NavBar />
+<div className='Container' >
+  <Forms />
+  </div>
+
+
+    </div>
+  );
+}
+
+
+
+
+export default App;
